45d19ee17d1cfbebf2a4b847ef7a52182b88bb95
Nurudy <114388816+Nurudy@users.noreply.github.com>
2024-01-16 08:10:12 +0100

    b

--- Assets/Scripts/PLAYER_MOV.cs
+++ Assets/Scripts/PLAYER_MOV.cs
@@ -8,6 +8,10 @@ public class PLAYER_MOV : MonoBehaviour
 
     public float speed;
     private float Move;
+    public float jumpHeight;
+    private bool isJumping = false; 
+
+
 
     public Rigidbody2D rig;
 
@@ -16,13 +20,31 @@ public class PLAYER_MOV : MonoBehaviour
         
     }
 
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
     private void Update()
     {
         Move = Input.GetAxis("Horizontal");
+        
 
         rig.velocity = new Vector2(speed * Move, rig.velocity.y);
+        
+        if(Input.GetKeyDown(KeyCode.Escape) && !isJumping)
+        {
+            rig.AddForce(Vector2.up * jumpHeight); // you need a reference to the RigidBody2D component
+            isJumping = true;
+        }
     }
-
+    /*private void OnCollisionEnter2D(Collision col)
+    {
+        if (col.gameObject.tag == "ground") // GameObject is a type, gameObject is the property
+        {
+            isJumping = false;
+        }
+    }*/
 
 
 }
